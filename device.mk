d46e502b115503c58aaa1a92988980f2e2eadf2d
mweninge <max.weninger@windriver.com>
2016-07-19 17:54:58 +0200

    hack a shaq
    Conflicts:
    	device.mk
    
    Change-Id: I6d49682c3a4733c10404a4f6db994e47bc692b21

--- device.mk
+++ device.mk
@@ -288,5 +288,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat
 
+# hack a shaq
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/netmgrd_control.sh:system/etc/netmgrd_control.sh
+
+
 # Inherit from oppo-common
 $(call inherit-product, device/oppo/common/common.mk)
